faa999b01180aa1bfb740f9ed6f22c65c7ef9a79
MD Abdul Zabber Islam Apurbo <md.abdul.zabber.islam.apurbo@g.bracu.ac.bd>
2023-10-21 14:13:08 +0600

    navbar design changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Nav2 from "./Nav2";
 
 export const Navbar = () => {
   return (
-    <header className="flex justify-between items-center p-5 backdrop-blur-3xl bg-transparent fixed top-0 right-0 left-0 z-10 md:p-5 lg:px-24 2xl:px-36 text-gray-700">
+    <header className="flex justify-between items-center bg-gray-200 fixed top-0 right-0 left-0 z-50 lg:px-24 2xl:px-36 text-gray-700 shadow-md">
       <div className="container mx-auto flex flex-wrap p-5 flex-row items-center">
         <div className="">
           <img src={Logo} className="w-24 lg:w-32 xl:w-40" />
